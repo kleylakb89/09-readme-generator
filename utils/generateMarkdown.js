1ec091b13e31796d9bf4210f2be3726f5e99213a
Kristin Kleyla <clever_disguises@yahoo.com>
2022-06-13 21:22:18 -0400

    wrote renderLicenseSection function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -26,12 +26,16 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  const links = renderLicenseLink(license);
+  
+  return (license === 'No License' ? '' : `This application is covered under the ${license} license. For more information, please see: [${license}](${links})`)
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseSection = renderLicenseSection(data.license);
   const badge = renderLicenseBadge(data.license);
-  const links = renderLicenseLink(data.license);
 
   return `# ${data.title}
 
