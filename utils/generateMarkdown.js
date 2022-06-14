6d69a7d0e36ac779531c0135d3a294b055821d50
Kristin Kleyla <clever_disguises@yahoo.com>
2022-06-13 21:12:56 -0400

    wrote renderLicenseLink function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,7 +9,20 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    'GNU AGPLv3': 'https://opensource.org/licenses/AGPL-3.0',
+    'GNU GPLv3': 'https://opensource.org/licenses/GPL-3.0',
+    'GNU LGPLv3': 'https://opensource.org/licenses/LGPL-3.0',
+    Mozilla: 'https://opensource.org/licenses/MPL-2.0',
+    Apache: 'https://www.apache.org/licenses/LICENSE-2.0',
+    MIT: 'https://opensource.org/licenses/MIT',
+    Boost: 'https://opensource.org/licenses/BSL-1.0',
+    'The Unlicense': 'https://unlicense.org/',
+    'No License': ''
+  }
+  return licenseLinks[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -18,6 +31,7 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   const badge = renderLicenseBadge(data.license);
+  const links = renderLicenseLink(data.license);
 
   return `# ${data.title}
 
